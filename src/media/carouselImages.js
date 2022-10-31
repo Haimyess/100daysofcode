41453ca7f297cb17a9acd593f9f0e4268cd49776
Haim Yeshurun <haimyess@gmail.com>
2022-10-31 09:32:43 +0200

    Changed relative route images carousel

--- src/media/carouselImages.js
+++ src/media/carouselImages.js
@@ -1,8 +1,8 @@
 /** @format */
 
-import comic_festival from "/Users/gyngee/Documents/100daysofcode/src/media/comic_festival.jpeg";
-import icon_festival from "/Users/gyngee/Documents/100daysofcode/src/media/Icon_festival.jpeg";
-import octoberfest from "/Users/gyngee/Documents/100daysofcode/src/media/octoberfest.jpeg";
-import tlv_marathon_23 from "/Users/gyngee/Documents/100daysofcode/src/media/tlv_marathon_23.jpeg";
+import comic_festival from "src/media/comic_festival.jpeg";
+import icon_festival from "src/media/Icon_festival.jpeg";
+import octoberfest from "src/media/octoberfest.jpeg";
+import tlv_marathon_23 from "src/media/tlv_marathon_23.jpeg";
 
 export default [comic_festival, icon_festival, octoberfest, tlv_marathon_23];
