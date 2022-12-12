ebb563942dd0f4e16e822903f825d25ae84fd54c
Haim Yeshurun <haimyess@gmail.com>
2022-12-12 16:55:21 +0200

    Show subnavbar onscroll

--- src/Components/OnScroll.js
+++ src/Components/OnScroll.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
@@ -8,6 +8,23 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import "../styles/onscroll.css";
 
 function OnScroll() {
+  const firstSectionRef = useRef();
+  const [firstSectionVisible, setFirstSectionVisible] = useState();
+  console.log(firstSectionVisible);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      //   const entry = entries[0];
+      console.log("entries", entries[0].target.clientHeight);
+      setFirstSectionVisible(entries[0].isIntersecting);
+    });
+    observer.observe(firstSectionRef.current);
+  }, []);
+
+  useEffect(() => {
+    // const
+  }, []);
+
   return (
     <div className='onscroll-container'>
       <header className='nav-container stick'>
@@ -24,34 +41,71 @@ function OnScroll() {
         </div>
         <Link className='logo-link' to='/'>
           <p className='logo'>OnScroll</p>
-          {/* <p className='logo'>100DaysCodingReact</p> */}
-          {/* <img src={{ logo }} alt='Logo' /> */}
         </Link>
         <nav>
           <ul className='list'>
             <li>About</li>
             <li>Categories</li>
             <li>Cart</li>
-            {/* <Link className='projects' to='/projects'>
-              Projects
-            </Link> */}
           </ul>
         </nav>
+        {firstSectionVisible !== true && (
+          <nav className='subnav'>
+            <div className='subnav-container'>
+              <ul className='subnav-list'>
+                <li className='subnav-item'>Info</li>
+                <li className='subnav-item'>Services</li>
+                <li className='subnav-item'>Dates</li>
+                <li className='subnav-item'>Location</li>
+              </ul>
+            </div>
+          </nav>
+        )}
       </header>
 
+      {/*) <header className='nav-container stick'>
+          <div style={{ display: "inline-block" }}>
+            <Link className='back-link' to='/projects'>
+              <div className='back-link-container'>
+                {" "}
+                <span>
+                  <ArrowBackIcon />
+                </span>
+                back to projects
+              </div>
+            </Link>
+          </div>
+          <Link className='logo-link' to='/'>
+            <p className='logo'>OnScroll</p>
+         
+          </Link>
+          <nav>
+            <ul className='list'>
+              <li>About</li>
+              <li>Categories</li>
+              <li>Cart</li>
+           
+            </ul>
+          </nav>
+        </header>
+      ) : (
+        <nav className='subnav'>
+          <div className='subnav-container'>
+            <ul className='subnav-list'>
+              <li className='subnav-item'>Info</li>
+              <li className='subnav-item'>Services</li>
+              <li className='subnav-item'>Dates</li>
+              <li className='subnav-item'>Location</li>
+            </ul>
+          </div>
+        </nav>
+      ) */}
+
       <main className='main-container'>
-        <section className='first-section section'>About</section>
+        <section ref={firstSectionRef} className='first-section section'>
+          <h2>About</h2>
+        </section>
 
-        {/* Sub navbar appearing after we get to the second section */}
-
-        {/* <nav className='subnav'>
-          <ul className='subnav-list'>
-            <li className='subnav-item'>Info</li>
-            <li className='subnav-item'>Services</li>
-            <li className='subnav-item'>Dates</li>
-            <li className='subnav-item'>Location</li>
-          </ul>
-        </nav> */}
         <section className='second-section section'>
           <section id='left'>
             <section>
@@ -60,41 +114,126 @@ function OnScroll() {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Reiciendis praesentium dolor numquam consequatur dolorum
                 voluptas corrupti fugiat est ea, nemo officiis suscipit
-                doloribus accusamus recusandae eveniet incidunt. Amet eius
-                impedit eligendi quis, veniam voluptatem perspiciatis obcaecati
-                consectetur rem cum maxime dicta ab modi sunt blanditiis
-                suscipit autem iste placeat deleniti! Reiciendis at illo
-                perferendis rem molestias quas in mollitia libero porro hic
-                provident saepe, veritatis, neque optio ut dolorem, earum eum
-                harum! Tempore id expedita corporis dignissimos dolorem tempora
-                voluptates, voluptas quod obcaecati beatae. Sed tempore ipsa
-                quos! Modi excepturi facilis sequi praesentium cum et quidem
-                molestias a. Libero eum, et maiores dignissimos sunt dolorem rem
-                officiis pariatur, laborum commodi a iste optio ea odio autem
-                sit. Minima doloremque voluptas voluptate placeat delectus
-                debitis blanditiis enim suscipit! Aliquam reiciendis inventore
-                ratione corrupti temporibus recusandae provident, numquam rem
-                quo excepturi quidem voluptatibus itaque aspernatur aliquid
-                nihil facilis ex ipsum placeat illum minima dolores fuga ipsa
-                exercitationem? Laudantium enim ullam, sint pariatur dolorem
-                quis necessitatibus nisi ipsam iusto, officiis dignissimos
-                voluptas. Similique maxime eaque optio excepturi natus sunt
-                officia? Repellat deserunt nostrum magni animi, voluptates
-                incidunt sapiente asperiores quibusdam suscipit eligendi fuga.
-                Molestiae harum illo nam sit culpa impedit natus sapiente
-                fugiat?
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum
+                praesentium magnam quia ducimus! Totam nulla fuga, officia iure
+                architecto consequuntur minus dicta illum facilis commodi quod
+                dignissimos pariatur, temporibus qui culpa blanditiis laborum
+                ullam minima non repudiandae, nihil quas recusandae hic placeat?
+                Dolore, eum natus tenetur est animi ratione accusamus id quia
+                eos unde quam eligendi architecto fugiat aperiam at doloremque
+              </p>
+              <p>
+                autem, temporibus voluptates doloribus rem! Magni quia et optio
+                accusamus voluptatibus labore unde facere sapiente libero nihil.
+                Labore nostrum eum blanditiis, est cum adipisci maxime dicta vel
+                eveniet temporibus facilis commodi ad molestiae quo itaque, rem,
+                sit laudantium nulla.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit Lorem
+                ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+                praesentium dolor numquam consequatur dolorum voluptas corrupti
+                fugiat est ea, nemo officiis suscipit
               </p>
             </section>
             <section>
               <h2>Services</h2>
+              <p>
+                autem, temporibus voluptates doloribus rem! Magni quia et optio
+                accusamus voluptatibus labore unde facere sapiente libero nihil.
+                Labore nostrum eum blanditiis, est cum adipisci maxime dicta vel
+                eveniet temporibus facilis commodi ad molestiae quo itaque, rem,
+                sit laudantium nulla.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum
+                praesentium magnam quia ducimus! Totam nulla fuga, officia iure
+                architecto consequuntur minus dicta illum facilis commodi quod
+                dignissimos pariatur, temporibus qui culpa blanditiis laborum
+                ullam minima non repudiandae, nihil quas recusandae hic placeat?
+                Dolore, eum natus tenetur est animi ratione accusamus id quia
+                eos unde quam eligendi architecto fugiat aperiam at doloremque
+                autem, temporibus voluptates doloribus rem! Magni quia et optio
+                accusamus voluptatibus labore unde facere sapiente
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit Lorem
+                ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+                praesentium dolor numquam consequatur dolorum voluptas corrupti
+                fugiat est ea, nemo officiis suscipit
+              </p>
             </section>
             <section>
               <h2>Dates</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit
+              </p>
+              <p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Reiciendis praesentium dolor numquam consequatur dolorum
+                  voluptas corrupti fugiat est ea, nemo officiis suscipit
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Reiciendis praesentium dolor numquam consequatur dolorum
+                  voluptas corrupti fugiat est ea, nemo officiis suscipit
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Reiciendis praesentium dolor numquam consequatur dolorum
+                  voluptas corrupti fugiat est ea, nemo officiis suscipit
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Reiciendis praesentium dolor numquam consequatur dolorum
+                  voluptas corrupti fugiat est ea, nemo officiis suscipit
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Reiciendis praesentium dolor numquam consequatur dolorum
+                  voluptas corrupti fugiat est ea, nemo officiis suscipit
+                </p>
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis praesentium dolor numquam consequatur dolorum
+                voluptas corrupti fugiat est ea, nemo officiis suscipit
+              </p>
             </section>
           </section>
           <aside id='right'>
             <div className='aside-container'>
               <p>Aside</p>
+              <button className='btn-reserve'>Reserve</button>
             </div>
           </aside>
         </section>
